feddc9d12a2ad0ea581479a98a0a6ec946383430
FacundoCuzziol <105680789+FacundoCuzziol@users.noreply.github.com>
2022-05-16 19:33:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,9 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#Picklist to choose the fruit to include
+streamlit.multiselect('Pick some fruits:',list(my_fruit_list).index))
+#Display the table on the page
 streamlit.dataframe(my_fruit_list)
